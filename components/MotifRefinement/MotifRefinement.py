270fd69b35fb9cd88c6bbaa00131cf5b321383bf
omidi <saeed.omidi@gmail.com>
2014-08-08 11:05:42 +0200

    The modified weblogo-3.4 is added to the scripts. In addition a minor changes is being made in MotifRefinement component to use the modified weblogo in order to generate the motifs.

--- components/MotifRefinement/MotifRefinement.py
+++ components/MotifRefinement/MotifRefinement.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import component_skeleton.main
 from motevo_stuff import run_motevo
+from sequence_logo import generate_sequence_logo
 import os
 
 def execute(cf):
@@ -13,10 +14,14 @@ def execute(cf):
     inputSequences = cf.get_input("InputSequences")
     wmFile = cf.get_input("WM")
     output_file = cf.get_output("RefinedMotif")
+    output_motif = cf.get_output("RefinedMotif.pdf")    
     genome = cf.get_parameter("genome", "string")
+    weblogo_path = cf.get_parameter("weblogo_path", "string")
     output_dir = os.path.dirname(output_file)
     (siteFilename, priorFilename, paramFilename) = run_motevo(wmFile, inputSequences, \
                                                output_file, output_dir, genome)
+    desc = os.path.basename(os.path.dirname(output_file))    
+    generate_sequence_logo(output_file, output_motif, weblogo_path, desc)
     os.system( 'rm %s %s %s' % (siteFilename, priorFilename, paramFilename) )
     return 0
 
