e8ede4e5dda8eecce627432e12baa20307eb3a8b
omidi <saeed.omidi@gmail.com>
2014-08-25 17:07:47 +0200

    The EnrichmentScore component is added that fits the parameters for each motif and finally calculate and report the std and mean values of the enrichment score for each motif. This component runs over a bulk of motifs, mainly de novo motifs and DB motifs. There is only one problem with Drmaa running bulk jobs or job arrays

---
+++ components/EnrichmentScores/#fitting_beta.py#
@@ -0,0 +1,76 @@
+import re, os
+import numpy as np
+
+
+def sum_of_posteriors_foreground_regions(fname):    
+    posteriors = {}
+    number_of_windows = {}
+    number_of_regions = {}
+    total_posterior = 0.
+    total_number_of_windows = 0.
+    with open(fname) as file_handler:
+        for line in file_handler:            
+            row = line.split()
+            posterior = float(row[2])
+            if re.search('_reg\d+', row[-1]):
+                posteriors.setdefault(row[-1].strip(), 0.0)
+                posteriors[row[-1].strip()] += posterior
+                number_of_windows.setdefault(row[-1].strip(), 0)
+                number_of_windows[row[-1].strip()] += 1
+            number_of_regions.setdefault(row[-1].strip(), 0)
+            total_posterior += posterior
+            total_number_of_windows += 1.
+    return posteriors, total_posterior, number_of_windows, total_number_of_windows, len(number_of_regions.keys())
+
+
+def likelihood_derivative_beta(normalized_sitecount, beta, N_over_L, B):
+    return beta + N_over_L - B*np.power(np.sum(1.0 / (beta + normalized_sitecount)) , -1 )
+
+
+def fit_beta(siteFile, interm_dir, wmFile):
+    binding_regions_sitecount, total_sitecount, number_of_windows, total_length, M = sum_of_posteriors_foreground_regions(siteFile)
+    beta_min, beta_max = 1.0e-12, 1.0e+3
+    step_size = 5.0
+    motifName = os.path.basename(wmFile)    
+    # res_file = open(os.path.join(interm_dir, '%s.beta' % motifName), 'w')    
+    beta_lower_bound, beta_upper_bound = beta_min, beta_min+step_size
+    N_over_L = total_sitecount / float(total_length)
+    B = len(binding_regions_sitecount.keys())
+    tmp = [] # it's defined to speed up the analysis
+    # instead of calculating \sum_{r \in B} \frac{l_r}{beta*l_r + n_r}
+    # I calculate: \sum_{r \in B} \frac{1}{beta + n_r/l_r}
+    # where n_r/l_r is like site per nucleotide for a sequence
+    for region, sitecount in binding_regions_sitecount.items():
+        tmp.append(sitecount / float(number_of_windows[region]))
+    normalized_sitecount = np.array(tmp)
+    # updating the lower and upper bounds for the value of Beta
+    # It's where the two bounds (upper and lower) have opposite signs
+    likelihood_beta_lower = likelihood_derivative_beta(normalized_sitecount, beta_lower_bound, N_over_L, B)
+    # res_file.write('%f\t%0.12f\n' % (beta_lower_bound, likelihood_beta_lower))
+    if likelihood_beta_lower > .0:
+        while True:            
+            likelihood_beta_upper = likelihood_derivative_beta(normalized_sitecount, beta_upper_bound, N_over_L, B)
+            if likelihood_beta_upper < 0.:
+                beta_lower_bound = beta_upper_bound - step_size
+                break
+            beta_upper_bound += step_size
+            # res_file.write('%f\t%0.12f\n' % (beta_upper_bound, likelihood_beta_upper))            
+            if beta_upper_bound > beta_max:  # prevents to go very very large numbers for Beta
+                return beta_max
+    else:  # once the derivative of likelihood for the lowest beta is already negative
+        return beta_lower_bound
+
+    # bisection method for refining the search area
+    while (beta_upper_bound - beta_lower_bound) > 1.0e-9:
+        mid_beta = (beta_upper_bound + beta_lower_bound) / 2.0
+        # print beta_lower_bound, mid_beta, beta_upper_bound
+        mid_likelihood = likelihood_derivative_beta(normalized_sitecount, mid_beta, N_over_L, B)
+        # print beta_lower_bound, mid_beta, beta_upper_bound, mid_likelihood        
+        if mid_likelihood > .0:
+            beta_lower_bound = mid_beta
+        else:
+            beta_upper_bound = mid_beta
+    # print mid_beta
+    # res_file.write('beta:\t%0.12f\n' % mid_beta)
+    # res_file.close()
+    return mid_beta
---
+++ components/EnrichmentScores/EnrichmentScores.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+import component_skeleton.main
+import os
+import subprocess
+import re 
+import drmaa
+from time import sleep
+
+OPATH=":"+drmaa.JobTemplate.HOME_DIRECTORY+'/DRMAA_JOB_OUT'
+
+def createScratchDirectory(outfile):
+    scratch_dir = os.path.join(os.path.dirname(outfile), "scratch")
+    os.system('mkdir %s' % scratch_dir)
+    return scratch_dir
+
+
+def listOfAllWMs(denovoWMs, databaseWMs, scratchDir):
+    WmFilename = os.path.join(scratchDir, 'WMs')
+    with open(WmFilename, 'w') as WMs:
+        for wm in os.listdir(denovoWMs):
+            if re.search('^denovo_WM_\d+$', wm):  # to make sure to only include the denovo motifs
+                WMs.write(os.path.join(denovoWMs, wm) + '\n')
+        for wm in os.listdir(databaseWMs):
+            WMs.write(os.path.join(databaseWMs, wm) + '\n')
+    return WmFilename
+
+
+def createSequencePool(InputSequences, DecoySequences, scratchDir, resFile):
+    resFilename = os.path.join(scratchDir, resFile)
+    cmd = ' '.join(['cat',
+        InputSequences,
+        DecoySequences])
+    proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    with open(resFilename, 'w') as outf:
+        for line in proc.stdout:
+            outf.write(line)
+    return resFilename
+
+
+def createJobTemplate(TrainingPool, TestSequences, WMs, scratchDir, genome):
+    prog = '/import/bc2/home/nimwegen/omidi/Projects/ChIPseq-pipeline/components/EnrichmentScores/calculate_enrichment_score.py'
+    jobFileContent = '\n'.join([
+        '#! /bin/bash',
+        'WMFILE=%s' % WMs,
+        'WM=$(awk "NR==$SGE_TASK_ID" $SEEDFILE)',
+        'python {prog} -w $WM \\'.format(prog=prog),
+        # 'python {prog} -w {WM} \\'.format(prog=prog, WM='/import/bc2/home/nimwegen/omidi/Projects/ChIPseq-pipeline/example/OUTPUT/CTCF_FgBg-filterwms/WMdir/denovo_WM_1'),        
+        '-t {trainseq} \\'.format(trainseq=TrainingPool),
+        '-s {testseq} \\'.format(testseq=TestSequences),        
+        '-o {scratch} -g {genome} '.format(scratch=scratchDir, genome=genome),
+        ])
+    shellFilename = os.path.join(scratchDir, 'command.sh')
+    shellFile = open(shellFilename, 'w')
+    shellFile.write(jobFileContent)
+    shellFile.close()
+    return shellFilename
+
+
+
+def init_job_template(jt, path, args, as_bulk_job, scratchDir):
+    stderr = os.path.join(scratchDir, 'stderr.$SGE_TASK_ID')
+    print stderr
+    JOB_PARAM = '-q fs_long -P project_nimwegen -e %s' % stderr
+    jt.workingDirectory = drmaa.JobTemplate.HOME_DIRECTORY
+    jt.remoteCommand = path
+    jt.nativeSpecification = JOB_PARAM
+    jt.args = args
+    jt.joinFiles=True
+    if as_bulk_job:
+        jt.outputPath=OPATH+'.'+drmaa.JobTemplate.PARAMETRIC_INDEX
+    else:
+        jt.outputPath=OPATH
+    return jt
+
+
+def runningDrmaaJob(job_path, scratchDir, JOB_CHUNK=5, NBULKS=1):
+    s=drmaa.Session()
+    s.initialize()
+    # submit bulk jobs
+    jt=init_job_template(s.createJobTemplate(), job_path, [], True, scratchDir)
+    all_jobids = []
+    for i in range(NBULKS):
+        all_jobids += s.runBulkJobs(jt, 1, JOB_CHUNK, 1)
+        sleep(1)
+    print all_jobids
+    return all_jobids
+    
+
+def execute(cf):
+    """
+    It receives two set of motifs: the de novo motifs, and the databse motifs.
+    For each of these motifs, it fits the parameters background prior and beta.
+    Using the fitted parameters, it calculates the average and sd enrichment scores
+    for each of the motifs.
+    Final result is a file that its line holds the value of prior, beta, mean
+    enrichment score, and standard deviation enrichment score for each motif. 
+    """
+    TrainingInputSequences = cf.get_input("TrainingSequences")
+    TrainingDecoySequences = cf.get_input("TrainingDecoySequences")
+    TestSequences = cf.get_input("TestSequences")
+    TestDecoySequences = cf.get_input("TestDecoySequences")
+    DenovoWMs = cf.get_input("DenovoWMs")    
+    DatabaseWMs = cf.get_input("DatabaseWMs")
+    GENOME = cf.get_parameter('genome', 'string')
+    outfile = cf.get_output("EnrichmentScores")
+    ## The scratch directory serves as a temporary space for holding files
+    scratchDir = createScratchDirectory(outfile)
+    ## create a file that lists all the input WMs    
+    WMs = listOfAllWMs(DenovoWMs, DatabaseWMs, scratchDir)
+    ## create the training pool that contains both real and decoy (shuffled) sequences
+    trainingPool = createSequencePool(TrainingInputSequences, TrainingDecoySequences, \
+                                      scratchDir, 'trainingPool')
+    testPool = createSequencePool(TestSequences, TestDecoySequences, \
+                                  scratchDir, 'testPool')
+    ## createJobTemplate for the array job (runs for every motif the fitting and enrichment score program)
+    shellCommand = createJobTemplate(trainingPool, testPool, WMs, scratchDir, GENOME)
+    runningDrmaaJob(shellCommand, scratchDir)
+    return 0
+
+component_skeleton.main.main(execute)
