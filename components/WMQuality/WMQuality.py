d058b2417ba60fccc93af220ec93fc06dd003d5c
severinb <severin.berger@stud.unibas.ch>
2014-09-04 10:29:53 +0200

    Modified WMQuality component.
    Removed bug that wrote WM_updated in wrong directories. Cleaned up some other parts of the component.

--- components/WMQuality/WMQuality.py
+++ components/WMQuality/WMQuality.py
@@ -9,11 +9,12 @@ import signal
 from pylab import *
 from scipy import interpolate
 
-def giveMotevoParamFile(mode, genome, wmlen, inter_dir, tag, aligned, ufemodel_path, ATfreq, GCfreq):
+def giveMotevoParamFile(genome, inter_dir, tag, aligned, ufemodel_path, ATfreq, GCfreq, emprior, bgorder, bgprior, UFEwmprior, site_bool, loglik_bool):
     """
     Returns a parameter file for motevo.
     """
 
+    ##UFE_models from genome_dict are not used anymore
     #UFEmodel_hg19 is UFE model for mammal species
     genome_dict = {}
     genome_dict['hg19'] = ['((((hg19:0.032973,rheMac2:0.057695):0.09821,mm9:0.352605):0.020666,(bosTau6:0.186713,(equCab2:0.107726,canFam2:0.150374):0.010431):0.032764):0.156024,monDom5:0.425899);', '/import/bc2/home/nimwegen/GROUP/hseq_pipeline/severin/Anduril/Pipeline/MotEvo_v1.0/UFEmodels/UFEmodel_hg19']
@@ -24,65 +25,76 @@ def giveMotevoParamFile(mode, genome, wmlen, inter_dir, tag, aligned, ufemodel_p
 
     sitefilepath = os.path.join(inter_dir, 'sites_' + tag)
     priorfilepath = os.path.join(inter_dir, 'priors_' + tag)
+    loglikfile = os.path.join(inter_dir, 'loglik_' + tag)
+
 
     print '\nCreate motevo parameter file %s' %tag
     print 'aligned', aligned
     if aligned:
         motevo_params = '\n'.join(['refspecies %s' %genome,
                                    'TREE %s' %genome_dict[genome][0],
-                                   'Mode %s' %mode,
-                                   'EMprior %s' %1,
+                                   'Mode TFBS',
+                                   'EMprior %s' %emprior,
                                    'priordiff %s' %0.05,
-                                   'UFEwmprior %s' %200,
+                                   'UFEwmprior %s' %UFEwmprior,
                                    'UFEwmfile %s' %ufemodel_path,
-                                   'UFEwmlen %s' %wmlen,
+                                   'UFEwmlen 10', #keep this constant, for several WMs it gets difficult anyway.
                                    'UFEprint %s' %0,
-                                   'markovorderBG %s' %1,
-                                   'bgprior %s' %0.99,
+                                   'markovorderBG %s' %bgorder,
+                                   'bgprior %s' %bgprior,
                                    'bg A %s' %ATfreq,
                                    'bg T %s' %ATfreq,
                                    'bg G %s' %GCfreq,
                                    'bg C %s' %GCfreq,
                                    'restrictparses %s' %0,
-                                   'sitefile %s' %sitefilepath,
                                    'priorfile %s' %priorfilepath,
                                    'printsiteals %s' %0,
-                                   'minposterior %f' %0.0])
+                                   'minposterior %f' %0.001])
+
+        if site_bool:
+            motevo_params += '\nsitefile %s' %sitefilepath
+        if loglik_bool:
+            motevo_params += '\nloglikfile %s' %loglikfile
+
     else:
         motevo_params = '\n'.join(['refspecies %s' %genome,
                                    'TREE (%s: 1)' %genome,
-                                   'Mode %s' %mode,
-                                   'EMprior %s' %1,
+                                   'Mode TFBS',
+                                   'EMprior %s' %emprior,
                                    'priordiff %s' %0.05,
-                                   'markovorderBG %s' %1,
-                                   'bgprior %s' %0.99,
-                                   # 'bg A %s' %0.25,
-                                   # 'bg T %s' %0.25,
-                                   # 'bg G %s' %0.25,
-                                   # 'bg C %s' %0.25,
+                                   'markovorderBG %s' %bgorder,
+                                   'bgprior %s' %bgprior,
+                                   'bg A %s' %ATfreq,
+                                   'bg T %s' %ATfreq,
+                                   'bg G %s' %GCfreq,
+                                   'bg C %s' %GCfreq,
                                    'restrictparses %s' %0,
-                                   'sitefile %s' %sitefilepath,
                                    'priorfile %s' %priorfilepath,
                                    'printsiteals %s' %0,
-                                   'minposterior %f' %0.0])            
+                                   'minposterior %f' %0.001])
+
+        if site_bool:
+            motevo_params += '\nsitefile %s' %sitefilepath
+        if loglik_bool:
+            motevo_params += '\nloglikfile %s' %loglikfile
+
 
     params_path = os.path.join(inter_dir, 'motevo_TFBS_params_' + tag)
     pf = open(params_path, 'w')
     pf.write(motevo_params)
-    return (params_path, sitefilepath)    
+    pf.close()
 
+    return (params_path, sitefilepath, priorfilepath, loglikfile)    
+    
 
     
-def runMotevo(motevo_path, alignments, params, WM, interm, tag):
+def runMotevo(motevo_path, seqs, params, WM, interm, tag):
     """
     runs Motevo
     """
     
-    pwd = os.getcwd()
-    os.chdir(interm)
-
     print '\nrun Motevo %s' %tag
-    proc = subprocess.Popen(motevo_path + ' %s %s \"%s\"' %(alignments, params, WM),  #> report%s %(tag)
+    proc = subprocess.Popen(motevo_path + ' %s %s \"%s\"' %(seqs, params, WM),
                             stdout=subprocess.PIPE,
                             stderr= subprocess.PIPE,
                             shell=True
@@ -90,14 +102,104 @@ def runMotevo(motevo_path, alignments, params, WM, interm, tag):
 
     stdout_value, stderr_value = proc.communicate()
     print stdout_value
-    print stderr_value
-    os.chdir(pwd)
+    print >> sys.stderr, stderr_value
     
     if proc.poll() > 0:
-        print '\tstderr:', repr(stderr_value.rstrip())
+        print >> sys.stderr, repr(stderr_value.rstrip())
         return -1
     else:
         return 0
+
+
+def predict_sites(train_set, test_set, bg_train_set, bg_test_set, tmpwm, ufemodel_path, ATfreq, GCfreq, interm, genome, motevo_path, aligned, tag_i, bg_prior, ufewm_prior):
+
+    ## 1. Fit prior on training set with EM
+    tag = 'fitP_' + tag_i
+    params, sites, priors, loglikfile = giveMotevoParamFile(genome, interm, tag, aligned, ufemodel_path, ATfreq, GCfreq, emprior=1, bgorder=0, bgprior=bg_prior, UFEwmprior=ufewm_prior, site_bool=False, loglik_bool=False)
+
+    tmpfile1 = os.path.join(interm, 'catted_' + tag_i)
+    os.system('cat %s %s > %s' %(train_set, bg_train_set, tmpfile1))
+
+    r = runMotevo(motevo_path, tmpfile1, params, tmpwm, interm, tag)
+    if r != 0:
+        print >> sys.stderr, 'motevo failed ', tag
+        sys.exit(1)
+
+    os.system('rm %s' %params)
+    os.system('rm %s' %tmpfile1)
+
+    # Read priors and add them into wm files
+    # prior file:
+    # WM_name final_prior nr_of_sites density
+    # /import/bc2/home/nimwegen/GROUP/hseq_pipeline/severin/Anduril/Pipeline/PipeLineSource/TESTRUN/NRF1_Z2/OUTPUT/NRF1_FgBg-runmotevoPG2_1/Logo 0.016554 635.008 0.251863
+    # background 0.983446 37724.8 0.748137
+    # UFEwm 0 0 0
+
+    prior_d = {}
+    for line in open(priors):
+        if line.startswith('WM_name'):
+            continue
+        t = line.strip().split()
+        prior_d[t[0]] = float(t[1])
+
+    os.system('rm %s' %priors)
+
+    tmpwm2 = os.path.join(interm, os.path.split(tmpwm)[1] + '_updated')
+    o = open(tmpwm2, 'w')
+    for line in open(tmpwm):
+        if line.startswith('NA'):
+            o.write(line)
+            t = line.strip().split()
+            try:
+                pri = prior_d[t[1]]
+            except KeyError:
+                prior_d[t[1]] = 0.0
+                pri = 0.0
+            o.write('PW\t%s\n' %(pri))
+        elif line.startswith('PW'):
+            continue
+        else:
+            o.write(line)
+    o.close()
+
+    try:
+        UFEwmprior = prior_d['UFEwm']
+    except KeyError:
+        UFEwmprior = 0
+
+    #print 'UFEwmprior: ', UFEwmprior
+    try:
+        bgprior = prior_d['background']
+    except KeyError, e:
+        print >> sys.stderr, 'bg_prior Issue: ', tag
+        print >> sys.stderr, e
+        print >> sys.stderr, prior_d
+        for i in open(priors):
+            print >> sys.stderr, 'l', i.rstrip()
+
+    os.system('rm %s' %priors)
+
+    ## 2. Compute log-likelihood on test set with optimal prior from training set and without EM
+    tag = 'fg_sites_' + tag_i
+    params, fg_sites, priors, loglikfile = giveMotevoParamFile(genome, interm, tag, aligned, ufemodel_path, ATfreq, GCfreq, emprior=0, bgorder=0, bgprior=bgprior, UFEwmprior=UFEwmprior, site_bool=True, loglik_bool=False)
+    runMotevo(motevo_path, test_set, params, tmpwm2, interm, tag)
+
+    os.system('rm %s' %params)
+    os.system('rm %s' %priors)
+
+    ## 3. Compute number of predicted binding sites on background set with optimal prior from training set and without EM
+    tag = 'bg_sites_' + tag_i
+    params, bg_sites, priors, loglikfile = giveMotevoParamFile(genome, interm, tag, aligned, ufemodel_path, ATfreq, GCfreq, emprior=0, bgorder=0, bgprior=bgprior, UFEwmprior=UFEwmprior, site_bool=True, loglik_bool=False)
+    runMotevo(motevo_path, bg_test_set, params, tmpwm2, interm, tag)
+
+
+    os.system('rm %s' %params)
+    os.system('rm %s' %tmpwm2)
+    os.system('rm %s' %priors)
+
+
+    return fg_sites, bg_sites
+
     
 
 def ROCanalysis(FGsitefiles, BGsitefiles, ROCplot, interm, peaks, bgPeaks, wm_names):
@@ -154,14 +256,17 @@ def ROCanalysis(FGsitefiles, BGsitefiles, ROCplot, interm, peaks, bgPeaks, wm_na
 
 def execute(cf):
     """
-    This function/component refines a given weight matrix, then predicts sites for old and refined WM and true sites and on background peaks.
-    With this it makes a positive predictive value - sensitivity plot.
-    It also makes a logo of the refined WM.
+    This component takes a bunch of WMs and then, for every single WM:
+        - fits a prior on the training pool (training set + shuffled training set)
+        - predicts TFBS using the fitted prior on the testing pool (testing set + shuffled testing set)
+        - plots a precision-recall curve using the site predictions
     """
 
     ##Ports and parameters
-    peaks = cf.get_input("peaks") #alignments or sequences
-    bgPeaks = cf.get_input("shuffledPeaks")
+    train_set = cf.get_input("train_set") #alignments or sequences
+    bg_train_set = cf.get_input("bg_train_set") #shuffled alignments or sequences
+    test_set = cf.get_input("test_set")
+    bg_test_set = cf.get_input("bg_test_set")
     WM1 = cf.get_input("WM1")
     WM2 = cf.get_input("WM2")
     WM3 = cf.get_input("WM3")
@@ -198,11 +303,13 @@ def execute(cf):
 
 
     if not aligned:
+        ufewm_prior = None
         ufemodel_path = None
-        ATfreq = None
-        GCfreq = None
+        ATfreq = 0.25
+        GCfreq = 0.25
     else:
-        #read base frequencies                                                                                                                                                                                                              
+        #read base frequencies
+        ufewm_prior = 200
         f = open(basefreqs)
         ATfreq = float(f.readline().strip().split()[1])
         GCfreq = float(f.readline().strip().split()[1])
@@ -230,22 +337,24 @@ def execute(cf):
 
         wmlen = len(open(WM).readlines())-4
         wm_names.append(os.path.split(WM)[1])
+        bg_prior = 0.99
 
-        #get sitefile for foregroung and background
-        tag = 'fg_%i' %i
-        (fgParams, fgSites) = giveMotevoParamFile('TFBS', genome, wmlen, interm, tag, aligned, ufemodel_path, ATfreq, GCfreq)
-        runMotevo(motevo_path, peaks, fgParams, WM, interm, tag)
-        FGsitefiles.append(fgSites)
+        #get sitefile for foreground and background
+        tag = 'wm_%i' %i
+        (fg_sites, bg_sites) = predict_sites(train_set, test_set, bg_train_set, bg_test_set, WM, ufemodel_path, ATfreq, GCfreq, interm, genome, motevo_path, aligned, tag, bg_prior, ufewm_prior)
 
-        tag = 'bg_%i' %i
-        (bgParams, bgSites) = giveMotevoParamFile('TFBS', genome, wmlen, interm, tag, aligned, ufemodel_path, ATfreq, GCfreq)
-        runMotevo(motevo_path, bgPeaks, bgParams, WM, interm, tag)
-        BGsitefiles.append(bgSites)
+        # (fgParams, fgSites) = giveMotevoParamFile('TFBS', genome, wmlen, interm, tag, aligned, ufemodel_path, ATfreq, GCfreq)
+        # runMotevo(motevo_path, peaks, fgParams, WM, interm, tag)
+        FGsitefiles.append(fg_sites)
+
+        # (bgParams, bgSites) = giveMotevoParamFile('TFBS', genome, wmlen, interm, tag, aligned, ufemodel_path, ATfreq, GCfreq)
+        # runMotevo(motevo_path, bgPeaks, bgParams, WM, interm, tag)
+        BGsitefiles.append(bg_sites)
 
 
     #ROC analysis
     print 'create ROC plot'
-    AUCs = ROCanalysis(FGsitefiles, BGsitefiles, ROCplot, interm, peaks, bgPeaks, wm_names)
+    AUCs = ROCanalysis(FGsitefiles, BGsitefiles, ROCplot, interm, test_set, bg_test_set, wm_names)
 
 
     l = open(log_file, 'w')
