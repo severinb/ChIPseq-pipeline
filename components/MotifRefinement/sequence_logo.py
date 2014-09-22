2d07c216ffa3ece1f2eade4b75a4a84151938fbf
omidi <saeed.omidi@gmail.com>
2014-09-22 16:42:00 +0200

    the weblogo part in MotifRefinment is changed to not have a PDF error

--- components/MotifRefinement/sequence_logo.py
+++ components/MotifRefinement/sequence_logo.py
@@ -8,10 +8,17 @@ def generate_sequence_logo(fname, resfile, weblogo_path, desc='', errorbars='NO'
         '--color-scheme classic',
         '--errorbars %s' % errorbars,
         '--fineprint %s' % desc,
+        '--format png', 
         '< %s >' % fname,
-        resfile
+        resfile.replace('.pdf', '.png')
         ])
-    print "Sequence Logo: "
-    print cmd 
+    # print "Sequence Logo: "
+    # print cmd 
+    os.system(cmd)
+    cmd = ' '.join([
+        'convert',
+        resfile.replace('.pdf', '.png'),
+        resfile, 
+        ])
     os.system(cmd)
     return 0
